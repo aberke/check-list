ef47d55dff15b3c465c63dda961627584e7d4aa4
Alexandra Berke <alexandra_berke@brown.edu>
2014-07-21 14:37:16 -0400

    bugherd tasks 31 and 38 complete.  In backstage: sorting cleaners by last_active.  Sorting lists by last_modified.  Showing notes and price in backstage.

--- app/backstage/static/backstage.js
+++ app/backstage/static/backstage.js
@@ -26,10 +26,6 @@ var App = angular.module('BackstageApp', [])
 		$scope.loading;
 		$scope.cleaners;
 		$scope.error;
-
-		$scope.$on('$viewContentLoaded', function() {
-			console.log('$viewContentLoaded')
-		});
 		
 	
 
@@ -46,8 +42,17 @@ var App = angular.module('BackstageApp', [])
 					var cleaner;
 					for (var i=0; i<$scope.cleaners.length; i++) {
 						cleaner = $scope.cleaners[i];
+						/* Sorting cleaners in ng-repeat by last_activity
+							where last_activity is more recent of 
+							cleaner.last_modified and 
+							most recent of their lists last_modified 
+						*/
+						cleaner.last_active = new Date(cleaner.last_modified);
 						for (var j=0; j<cleaner.lists.length; j++) {
 							cleaner.lists[j].last_modified = new Date(cleaner.lists[j].last_modified);
+							if (cleaner.lists[j].last_modified > cleaner.last_active) {
+								cleaner.last_active = cleaner.lists[j].last_modified;
+							}
 						}
 					}
 					$scope.loading = false;
