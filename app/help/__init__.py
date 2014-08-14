5668f75943799ea00b2ffd8b753fc7da87de5805
Alexandra Berke <alexandra_berke@brown.edu>
2014-08-14 13:46:04 -0400

    description meta tag in base.html now filled Help url endpoints set up

--- app/help/__init__.py
+++ app/help/__init__.py
@@ -25,3 +25,36 @@ bp = Blueprint('help', __name__, static_folder='static')
 @bp.route('/')
 def index():
 	return send_file('help/static/index.html')
+
+@bp.route('/create-an-account/')
+def create_an_account():
+	return send_file('help/static/create-an-account.html')
+
+@bp.route('/menu/')
+def menu():
+	return send_file('help/static/menu.html')
+
+@bp.route('/create-a-new-list/')
+def create_a_new_list():
+	return send_file('help/static/create-a-new-list.html')
+
+@bp.route('/finished/')
+def finished():
+	return send_file('help/static/finished.html')
+
+@bp.route('/process/')
+def process():
+	return send_file('help/static/process.html')
+
+@bp.route('/delete-a-list/')
+def delete_a_list():
+	return send_file('help/static/delete-a-list.html')
+
+@bp.route('/sign-into-your-account/')
+def sign_into_your_account():
+	return send_file('help/static/sign-into-your-account.html')
+
+@bp.route('/reset-your-password/')
+def reset_your_password():
+	return send_file('help/static/reset-your-password.html')
+
