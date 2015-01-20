fe0ab373f8e7f36e85b8958a4faa727444b1af9c
Bill Cromie <bill@cromie.org>
2015-01-20 15:15:37 -0500

    keeping the translations in a yaml file in the repository.
    using underscores for all translation keys.
    
    adding yaml as a requirement. json was finicky and less
    human readable.

--- app/language/map.py
+++ app/language/map.py
@@ -23,6 +23,12 @@
 # 	... for row/keyname in spreadsheet
 # }
 #
+
+# building the map.yaml file
+# grep for keys in the source:
+# `grep -R \{\{ '(.*)' \| translate . | grep -ow '[A-Z_]\+' | sort | uniq >> keys.txt`
+# and then build dictionary with the key as the en: and es: values.
+# then go through app and replace the en with the appropriate english.
 #	Google documentation for accessing spreadsheet data:
 #		https://developers.google.com/gdata/samples/spreadsheet_sample
 #
@@ -30,18 +36,17 @@
 #--------------------------------------------------------------------------------
 #*********************************************************************************
 
-import requests
-import re # splitting content returned from google with a regex
+#import requests
 
-from app.lib.util import APIexception, yellERROR, jsonp
-import config
+import os
+import yaml
 
 
 # Google documentation for accessing spreadsheet data:
 # https://developers.google.com/gdata/samples/spreadsheet_sample
-KEY = config.GOOGLE_API_TRANSLATE_SPREADSHEET_KEY 
-FORMAT = 'json'
-GOOGLE_API_REQUEST_URL = 'http://spreadsheets.google.com/feeds/list/{0}/od6/public/basic?alt={1}'.format(KEY, FORMAT)
+# KEY = config.GOOGLE_API_TRANSLATE_SPREADSHEET_KEY
+# FORMAT = 'json'
+# GOOGLE_API_REQUEST_URL = 'http://spreadsheets.google.com/feeds/list/{0}/od6/public/basic?alt={1}'.format(KEY, FORMAT)
 
 
 
@@ -56,49 +61,54 @@ def get_map():
 def build_map():
 	# re-initialize map
 	map = {}
+	script_dir = os.path.dirname(__file__)
+	rel_path = "./map.yaml"
+	map_path = os.path.join(script_dir, rel_path)
+	yaml_string = open(map_path)
 
-	# get translate data from spreadsheet as json
-	r = requests.get(GOOGLE_API_REQUEST_URL)
-	if not (r.status_code == 200 or r.status_code == '200'):
-		raise APIexception(message='Error retrieving translate google spreadsheet')
+	map = yaml.load(yaml_string)
 
-	# parse translate data json into map form
+	yaml_string.close()
+	# # get translate data from spreadsheet as json
+	# r = requests.get(GOOGLE_API_REQUEST_URL)
+	# if not (r.status_code == 200 or r.status_code == '200'):
+	# 	raise APIexception(message='Error retrieving translate google spreadsheet')
 
-	data = r.json()
+	# # parse translate data json into map form
 
-	entry_list = data['feed']['entry']
-	entry = None
-	keyname = None
-	content = None
-	translation_array = None
-	translation_partition = None
+	# data = r.json()
 
-	for i in range(len(entry_list)):
-		# will add { keyname: translations } to map after building translations dictionary
-		translations = {} # of form {'en': translation, 'es':translation,..}
+	# entry_list = data['feed']['entry']
+	# entry = None
+	# keyname = None
+	# content = None
+	# translation_array = None
+	# translation_partition = None
 
-		try:
-			entry = entry_list[i]
-			keyname = entry['title']['$t']
+	# for i in range(len(entry_list)):
+	# 	# will add { keyname: translations } to map after building translations dictionary
+	# 	translations = {} # of form {'en': translation, 'es':translation,..}
 
-			# content of form "en: phone, es: phonenumber abbr. in spanish"
-			content = entry['content']['$t']
-			translation_array = re.split(r', *', content)
+	# 	try:
+	# 		entry = entry_list[i]
+	# 		keyname = entry['title']['$t']
 
-			for a in range(len(translation_array)):
-				translation_partition = re.split(r': *', translation_array[a])
-				language = translation_partition[0]
-				translation = translation_partition[1]
-				translations[language] = translation
-		
-		except Exception as e:
-			yellERROR('Error parsing translation map item: \n' + e.message + '\n' + str(entry))
-			continue
+	# 		# content of form "en: phone, es: phonenumber abbr. in spanish"
+	# 		content = entry['content']['$t']
+	# 		translation_array = re.split(r', *', content)
 
-		map[keyname] = translations
+	# 		for a in range(len(translation_array)):
+	# 			translation_partition = re.split(r': *', translation_array[a])
+	# 			language = translation_partition[0]
+	# 			translation = translation_partition[1]
+	# 			translations[language] = translation
+
+	# 	except Exception as e:
+	# 		yellERROR('Error parsing translation map item: \n' + e.message + '\n' + str(entry))
+	# 		continue
+
+	# 	map[keyname] = translations
 
 	return map
-
-
 
 map = build_map()
